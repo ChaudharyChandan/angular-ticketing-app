8f7fe69e79dab24beaa3a4e4933c195c15cfff76
Chandan Kumar <chandankmr059@gmail.com>
2016-09-10 00:53:50 +0530

    - Change the db collection configuration to connect to mongolab db instance

--- connection.js
+++ connection.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-module.exports = mongoose.connect('mongodb://localhost/ticketapp');
+module.exports = mongoose.connect('mongodb://callhub:callhub123@ds029106.mlab.com:29106/ticketapp');
